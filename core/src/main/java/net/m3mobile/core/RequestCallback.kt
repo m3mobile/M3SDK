37d6d241bfb9f04c3240a77c04f492f033c1b4ff
Thirfir <thirfir@naver.com>
2025-11-11 13:45:09 +0900

    docs: RequestCallback KDoc 작성

--- core/src/main/java/net/m3mobile/core/RequestCallback.kt
+++ core/src/main/java/net/m3mobile/core/RequestCallback.kt
@@ -8,6 +8,15 @@ import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 
 fun interface RequestCallback<T: Any> {
+    /**
+     * Called when the asynchronous operation is complete.
+     *
+     * This function will be invoked on the main thread.
+     * Exactly one of the parameters will be non-null.
+     *
+     * @param result The successful result of the operation, or null if an error occurred.
+     * @param error An exception representing the error, or null if the operation was successful.
+     */
     fun onComplete(result: T?, error: Exception?)
 }
 
