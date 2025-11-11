c99ae7bffc40057252acf5e7251489c25113be0a
Thirfir <thirfir@naver.com>
2025-11-11 15:21:00 +0900

    docs: SDK 진입점 클래스 KDoc 내용 수정

--- sdk/startup/src/main/java/net/m3mobile/sdk/startup/M3Startup.kt
+++ sdk/startup/src/main/java/net/m3mobile/sdk/startup/M3Startup.kt
@@ -3,14 +3,19 @@ package net.m3mobile.sdk.startup
 import android.content.Context
 
 /**
- * A singleton object that provides access to the M3Startup SDK.
- * This object serves as the main entry point for interacting with the M3 device's startup settings.
+ * Provides access to the M3 StartUp SDK.
+ * This serves as the main entry point for interacting with the M3 StartUp settings.
  *
  * Use [M3StartUp.instance] to get a concrete implementation of the [M3StartUpSdk] interface.
  */
 @Suppress("DEPRECATION_ERROR")
 object M3StartUp {
 
+    /**
+     * Provides access to the M3 StartUp SDK.
+     * This is the main entry point for interacting with the M3 StartUp settings
+     * and is automatically initialized by the M3 library.
+     */
     @JvmStatic
     lateinit var instance: M3StartUpSdk
         private set
