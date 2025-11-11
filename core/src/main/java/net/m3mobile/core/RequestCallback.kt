eeeaa42f430738ee9fa527568cd6c6d9e9f119cb
Thirfir <thirfir@naver.com>
2025-11-11 14:33:03 +0900

    chore: 유틸리티 코드 분리

--- core/src/main/java/net/m3mobile/core/RequestCallback.kt
+++ core/src/main/java/net/m3mobile/core/RequestCallback.kt
@@ -1,13 +1,7 @@
 package net.m3mobile.core
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.job
-import kotlinx.coroutines.launch
-
 fun interface RequestCallback<T: Any> {
+
     /**
      * Called when the asynchronous operation is complete.
      *
@@ -18,15 +12,4 @@ fun interface RequestCallback<T: Any> {
      * @param error An exception representing the error, or null if the operation was successful.
      */
     fun onComplete(result: T?, error: Exception?)
-}
-
-@JvmSynthetic
-@InternalM3Api
-fun launchOnMain(task: suspend () -> Unit): Job {
-    val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-
-    scope.launch {
-        task()
-    }
-    return scope.coroutineContext.job
 }
