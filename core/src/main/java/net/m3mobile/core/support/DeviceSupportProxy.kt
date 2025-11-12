6409c22af450e1ff33f013e0dae8905a92642997
Thirfir <thirfir@naver.com>
2025-11-12 10:37:25 +0900

    feat: API 프록시 객체 생성 객체 구현

---
+++ core/src/main/java/net/m3mobile/core/support/DeviceSupportProxy.kt
@@ -0,0 +1,37 @@
+package net.m3mobile.core.support
+
+import net.m3mobile.core.InternalM3Api
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Proxy
+
+@InternalM3Api
+object DeviceSupportProxy {
+
+    /**
+     * 주어진 인터페이스 `T`에 대한 동적 프록시를 생성합니다.
+     *
+     * 이 함수는 인터페이스의 모든 메서드 호출을 가로챕니다. 실제 구현체의 메서드를 호출하기 전에,
+     * [DeviceSupportAsserter.assertIsDeviceSupported]를 사용하여 현재 기기가 해당 기능을 지원하는지 먼저 확인합니다.
+     * 지원되지 않는 기기일 경우 예외가 발생합니다.
+     *
+     * @param T 프록시를 생성할 인터페이스 타입.
+     * @param implFactory `T`의 실제 구현 객체를 제공하는 람다 함수.
+     * @return `T` 타입의 프록시 객체.
+     */
+    @JvmSynthetic
+    inline fun <reified T: Any> create(implFactory: () -> T): T {
+        val interfaceClass = T::class.java
+        val impl = implFactory()
+        val handler = InvocationHandler { _, method, args ->
+            DeviceSupportAsserter.assertIsDeviceSupported(method)
+
+            method(impl, *(args ?: emptyArray()))
+        }
+
+        return Proxy.newProxyInstance(
+            interfaceClass.classLoader,
+            arrayOf(interfaceClass),
+            handler
+        ) as T
+    }
+}
