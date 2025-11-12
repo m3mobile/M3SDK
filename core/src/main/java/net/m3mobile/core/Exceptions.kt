597dac11cfc681a87fbf9cb90634ef187fdbe813
Thirfir <thirfir@naver.com>
2025-11-12 10:35:07 +0900

    feat: 지원하지 않는 모델 커스텀 예외 생성

---
+++ core/src/main/java/net/m3mobile/core/Exceptions.kt
@@ -0,0 +1,16 @@
+package net.m3mobile.core
+
+import android.os.Build
+
+/**
+ * Exception thrown when a feature is attempted to be used on an unsupported device model.
+ *
+ * This exception indicates that the feature is specific to certain
+ * M3 Mobile device models and is not available on the current device.
+ */
+class UnsupportedDeviceModelException(message: String) : RuntimeException(message) {
+    constructor(methodName: String, supportedModels: Array<out String>) : this(
+        "\"$methodName\" is not available on the current device (${Build.MODEL}). " +
+        "Supported models are: ${supportedModels.joinToString(", ")}."
+    )
+}
