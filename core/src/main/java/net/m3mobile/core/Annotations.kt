dd439871e6aee00c6857a15e3fb641c559dcef52
Thirfir <thirfir@naver.com>
2025-11-12 10:35:35 +0900

    feat: 지원하지 않는 모델 판별용 메서드 타겟 어노테이션 생성

--- core/src/main/java/net/m3mobile/core/Annotations.kt
+++ core/src/main/java/net/m3mobile/core/Annotations.kt
@@ -7,3 +7,20 @@ package net.m3mobile.core
     message = "This API is for internal use within the M3SDK. Do not call it directly."
 )
 annotation class InternalM3Api
+
+/**
+ * Specifies the M3 Mobile device models that a particular function supports.
+ *
+ * This annotation is used internally by the M3SDK to check if a function is compatible with the
+ * current device model. If the device model is not in the list of supported models,
+ * an [UnsupportedDeviceModelException] will be thrown when the function is called.
+ *
+ * Functions without this annotation are considered to be callable on all models.
+ *
+ * @property models An array of model names that support the annotated function.
+ * @see UnsupportedDeviceModelException
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+@InternalM3Api
+annotation class SupportedModels(vararg val models: String)
