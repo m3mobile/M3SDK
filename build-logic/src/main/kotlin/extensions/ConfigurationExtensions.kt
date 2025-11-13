87ea8af2c0d684f6fdea5a7f0c5b31ea9d80371d
Thirfir <thirfir@naver.com>
2025-11-13 11:41:56 +0900

    chore: 프로젝트 코틀린, 안드로이드 기본 설정 확장함수 추가

---
+++ build-logic/src/main/kotlin/extensions/ConfigurationExtensions.kt
@@ -0,0 +1,38 @@
+package extensions
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+internal val Project.catalog: VersionCatalog
+    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>
+) {
+    commonExtension.apply {
+        compileSdk = catalog.findVersion("projectCompileSdk").get().toString().toInt()
+        defaultConfig.minSdk = catalog.findVersion("projectMinSdk").get().toString().toInt()
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    configureKotlin()
+}
+
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+}
