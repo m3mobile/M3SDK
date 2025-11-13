2b895652e843a46d45b1dc66515242cc0d0cfb98
Thirfir <thirfir@naver.com>
2025-11-13 15:48:28 +0900

    feat: 오른쪽 스캔 키 WakeUp 설정 브로드캐스트 요청자 구현

--- sdk/keytool/src/main/java/net/m3mobile/sdk/keytool/constants/RequestAction.kt
+++ sdk/keytool/src/main/java/net/m3mobile/sdk/keytool/constants/RequestAction.kt
@@ -3,4 +3,5 @@ package net.m3mobile.sdk.keytool.constants
 internal object RequestAction {
 
     const val LEFT_SCAN_WAKEUP = "net.m3.keytool.WAKEUP_CONTROL_LEFT"
+    const val RIGHT_SCAN_WAKEUP = "net.m3.keytool.WAKEUP_CONTROL_RIGHT"
 }
---
+++ sdk/keytool/src/main/java/net/m3mobile/sdk/keytool/requester/wakeup/DisableRightScanWakeUpRequester.kt
@@ -0,0 +1,14 @@
+package net.m3mobile.sdk.keytool.requester.wakeup
+
+import android.content.Context
+import androidx.core.os.bundleOf
+import net.m3mobile.core.BroadcastRequester
+import net.m3mobile.sdk.keytool.constants.ExtraKey
+import net.m3mobile.sdk.keytool.constants.RequestAction
+
+internal class DisableRightScanWakeUpRequester(override val context: Context) : BroadcastRequester() {
+    override val requestAction = RequestAction.RIGHT_SCAN_WAKEUP
+    override val extras = bundleOf(
+        ExtraKey.ENABLE_WAKEUP to false
+    )
+}
