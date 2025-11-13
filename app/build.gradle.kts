e104f9d5c59c0b66b6ec5d4a4cc4c1e2166dfd0e
Thirfir <thirfir@naver.com>
2025-11-13 15:43:51 +0900

    chore: app모듈 커밋
    실제 라이브러리 배포에는 필요없으나 프로젝트 내 테스트 용으로 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
@@ -42,6 +42,10 @@ android {
 }
 
 dependencies {
+
+    implementation(projects.sdk.startup)
+    implementation(projects.sdk.keytool)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
