e82372918c4f286f104ce85d7b2e3d97f82e1aa9
Thirfir <thirfir@naver.com>
2025-11-13 17:53:47 +0900

    fix: 지원하지 않는 모델 예외 에러 메시지에서 모델명을 공식 모델명으로 출력하도록 수정

--- core/src/main/java/net/m3mobile/core/Exceptions.kt
+++ core/src/main/java/net/m3mobile/core/Exceptions.kt
@@ -1,6 +1,6 @@
 package net.m3mobile.core
 
-import android.os.Build
+import net.m3mobile.core.device.DeviceSupportAsserter
 
 /**
  * Exception thrown when a feature is attempted to be used on an unsupported device model.
@@ -10,7 +10,7 @@ import android.os.Build
  */
 class UnsupportedDeviceModelException(message: String) : RuntimeException(message) {
     constructor(methodName: String, supportedModels: Array<out String>) : this(
-        "\"$methodName\" is not available on the current device (${Build.MODEL}). " +
+        "\"$methodName\" is not available on the current device (${DeviceSupportAsserter.currentDeviceModel.name}). " +
         "Supported models are: ${supportedModels.joinToString(", ")}."
     )
 }
