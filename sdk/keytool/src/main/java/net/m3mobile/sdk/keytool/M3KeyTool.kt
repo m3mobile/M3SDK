98e1294cba1ca5cf4a25ba87330d1af2d42b26ba
Thirfir <thirfir@naver.com>
2025-11-13 18:11:15 +0900

    docs: M3KeyTool object KDoc 작성

--- sdk/keytool/src/main/java/net/m3mobile/sdk/keytool/M3KeyTool.kt
+++ sdk/keytool/src/main/java/net/m3mobile/sdk/keytool/M3KeyTool.kt
@@ -2,9 +2,21 @@ package net.m3mobile.sdk.keytool
 
 import android.content.Context
 
+/**
+ * Provides access to the M3 KeyTool SDK.
+ * This serves as the main entry point for interacting with the M3 KeyTool settings.
+ *
+ * Use [M3KeyTool.instance] to get a concrete implementation of the [M3KeyToolSdk] interface.
+ */
 @Suppress("DEPRECATION_ERROR")
 object M3KeyTool {
 
+    /**
+     * Provides access to the M3 KeyTool SDK.
+     *
+     * This is the main entry point for interacting with the M3 KeyTool settings
+     * and is automatically initialized by the M3 library.
+     */
     @JvmStatic
     lateinit var instance: M3KeyToolSdk
         private set
