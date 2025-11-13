a8dc0b31cf93a76d418eb169f1bbe01c3eb7e6d3
Thirfir <thirfir@naver.com>
2025-11-13 11:42:24 +0900

    feat: SDK 공통 의존성 플러그인 구현

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,3 +7,12 @@ dependencies {
     implementation(libs.kotlin.gradlePlugin)
     implementation(libs.kotlin.compiler.embeddable)
 }
+
+gradlePlugin {
+    plugins {
+        register("sdkCommonPlugin") {
+            id = "net.m3mobile.plugin.sdk.common"
+            implementationClass = "plugins.SdkCommonPlugin"
+        }
+    }
+}
---
+++ build-logic/src/main/kotlin/plugins/SdkCommonPlugin.kt
@@ -0,0 +1,59 @@
+package plugins
+
+import com.android.build.api.dsl.LibraryExtension
+import extensions.api
+import extensions.catalog
+import extensions.configureKotlinAndroid
+import extensions.implementation
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.project
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+class SdkCommonPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            tasks.withType<KotlinCompile>().configureEach {
+                compilerOptions {
+                    freeCompilerArgs.add("-opt-in=net.m3mobile.core.InternalM3Api")
+                }
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+
+                defaultConfig {
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+
+                buildTypes {
+                    release {
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+            }
+
+            afterEvaluate {
+                dependencies {
+                    api(project(":core"))
+
+                    implementation(catalog.findLibrary("startup-runtime").get())
+                    implementation(catalog.findLibrary("androidx-core-ktx").get())
+                }
+            }
+        }
+    }
+}
