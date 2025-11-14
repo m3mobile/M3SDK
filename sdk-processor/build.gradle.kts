2e885e7395062e7748f42110e1b14288c60adc1c
Thirfir <thirfir@naver.com>
2025-11-14 10:31:42 +0900

    chore: KSP 개발에 필요한 의존성 추가 (ksp-api, kotlin poet)

--- sdk-processor/build.gradle.kts
+++ sdk-processor/build.gradle.kts
@@ -1,15 +1,21 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    kotlin("jvm")
 }
+
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
 kotlin {
     compilerOptions {
         jvmTarget = JvmTarget.JVM_1_8
     }
+}
+
+dependencies {
+    implementation(libs.ksp.api)
+    implementation(libs.kotlin.poet)
 }
