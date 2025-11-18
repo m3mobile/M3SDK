0e2a1babe8e462c4f970551d69da725546cd6cf2
Thirfir <thirfir@naver.com>
2025-11-18 17:34:17 +0900

    refactor: Open Network 설정 API 리팩토링
    - Enable/Disable을 묶는 추상 클래스 생성

--- sdk/startup/src/main/java/net/m3mobile/sdk/startup/requester/wifi/SetOpenNetworkNotiStateRequester.kt
+++ sdk/startup/src/main/java/net/m3mobile/sdk/startup/requester/wifi/SetOpenNetworkNotiStateRequester.kt
@@ -7,10 +7,21 @@ import net.m3mobile.sdk.startup.constants.RequestAction
 import net.m3mobile.sdk.startup.constants.TypeKey
 import net.m3mobile.sdk.startup.constants.TypeValue
 
-internal class EnableOpenNetworkNotiRequester(override val context: Context): BroadcastRequester() {
+internal abstract class SetOpenNetworkNotiStateRequester: BroadcastRequester() {
 
     override val requestAction = RequestAction.CONFIG
     override val typeKey = TypeKey.SETTING
     override val typeValue = TypeValue.WIFI_OPEN_NOTIFICATION
-    override val extras = bundleOf("value" to 1)
+    override val extras = bundleOf("value" to value)
+    protected abstract val value: Int
+}
+
+internal class EnableOpenNetworkNotiRequester(override val context: Context): SetOpenNetworkNotiStateRequester() {
+
+    override val value = 1
+}
+
+internal class DisableOpenNetworkNotiRequester(override val context: Context): SetOpenNetworkNotiStateRequester() {
+
+    override val value = 0
 }
