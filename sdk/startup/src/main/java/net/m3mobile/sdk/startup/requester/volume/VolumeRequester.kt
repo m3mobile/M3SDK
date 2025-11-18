92e639c49cf7f817e0a299923d9727242ba92ae9
Thirfir <thirfir@naver.com>
2025-11-18 17:54:30 +0900

    refactor: 볼륨 설정 API 리팩토링
    - 동일한 추상클래스로 관리

---
+++ sdk/startup/src/main/java/net/m3mobile/sdk/startup/requester/volume/VolumeRequester.kt
@@ -0,0 +1,57 @@
+package net.m3mobile.sdk.startup.requester.volume
+
+import android.content.Context
+import androidx.core.os.bundleOf
+import net.m3mobile.core.requester.BroadcastRequester
+import net.m3mobile.sdk.startup.constants.RequestAction
+import net.m3mobile.sdk.startup.constants.TypeKey
+import net.m3mobile.sdk.startup.constants.TypeValue
+
+internal abstract class VolumeRequester: BroadcastRequester() {
+
+    override val requestAction = RequestAction.SYSTEM
+    override val typeKey = TypeKey.SETTING
+    override val typeValue = TypeValue.VOLUME
+}
+
+internal class DisableVibrationModeRequester(override val context: Context): VolumeRequester() {
+
+    override val extras = bundleOf("volume_vibrator" to false)
+}
+
+internal class EnableVibrationModeRequester(override val context: Context): VolumeRequester() {
+
+    override val extras = bundleOf("volume_vibrator" to true)
+}
+
+internal class SetAlarmVolumeRequester(
+    override val context: Context,
+    value: Int
+): VolumeRequester() {
+
+    override val extras = bundleOf("volume_alarm" to value)
+}
+
+internal class SetMediaVolumeRequester(
+    override val context: Context,
+    value: Int
+): VolumeRequester() {
+
+    override val extras = bundleOf("volume_media" to value)
+}
+
+internal class SetNotificationVolumeRequester(
+    override val context: Context,
+    value: Int
+): VolumeRequester() {
+
+    override val extras = bundleOf("volume_notification" to value)
+}
+
+internal class SetRingtoneVolumeRequester(
+    override val context: Context,
+    value: Int
+): VolumeRequester() {
+
+    override val extras = bundleOf("volume_ringtone" to value)
+}
