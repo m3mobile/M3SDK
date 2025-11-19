0531267c50da4f520be992d168697538a89df144
Thirfir <thirfir@naver.com>
2025-11-19 10:27:28 +0900

    fix: USB 모드 설정 API에 지원 기기 추가

--- sdk/startup/src/main/java/net/m3mobile/sdk/startup/api/UsbApi.kt
+++ sdk/startup/src/main/java/net/m3mobile/sdk/startup/api/UsbApi.kt
@@ -1,29 +1,37 @@
 package net.m3mobile.sdk.startup.api
 
+import net.m3mobile.core.SupportedModels
+import net.m3mobile.core.device.DeviceModel
+
 interface UsbApi {
 
     /**
      * Sets the USB connection mode to MTP.
      */
+    @SupportedModels(DeviceModel.US20, DeviceModel.US30)
     fun setUsbModeMtp()
 
     /**
      * Sets the USB connection mode to RNDIS.
      */
+    @SupportedModels(DeviceModel.US20, DeviceModel.US30)
     fun setUsbModeRndis()
 
     /**
      * Sets the USB connection mode to MIDI.
      */
+    @SupportedModels(DeviceModel.US20, DeviceModel.US30)
     fun setUsbModeMidi()
 
     /**
      * Sets the USB connection mode to PTP.
      */
+    @SupportedModels(DeviceModel.US20, DeviceModel.US30)
     fun setUsbModePtp()
 
     /**
      * Disables all USB data connections, setting the mode to charging only.
      */
+    @SupportedModels(DeviceModel.US20, DeviceModel.US30)
     fun setUsbModeNone()
 }
