f3fc8b0a67cc8b5910c38b79dc2d5dfc4f1dc0c3
Thirfir <thirfir@naver.com>
2025-11-28 14:02:22 +0900

    docs: 권한 API Docs 작성

--- sdk/startup/src/main/java/net/m3mobile/sdk/startup/api/PermissionApi.kt
+++ sdk/startup/src/main/java/net/m3mobile/sdk/startup/api/PermissionApi.kt
@@ -2,7 +2,19 @@ package net.m3mobile.sdk.startup.api
 
 interface PermissionApi {
 
+    /**
+     * Grants a specific runtime permission to a target application package.
+     *
+     * @param packageName The unique package identifier of the application to which the permission will be granted
+     * @param permission The fully qualified name of the permission to be granted
+     */
     fun grantPermission(packageName: String, permission: String)
 
+    /**
+     * Revokes a specific runtime permission to a target application package.
+     *
+     * @param packageName The unique package identifier of the application to which the permission will be revoked
+     * @param permission The fully qualified name of the permission to be revoked
+     */
     fun revokePermission(packageName: String, permission: String)
 }
