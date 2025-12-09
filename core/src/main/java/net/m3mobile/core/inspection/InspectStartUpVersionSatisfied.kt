f131db80c3e6fc8a881957139135c06027dd5dc6
Thirfir <thirfir@naver.com>
2025-12-09 09:02:51 +0900

    chore: StartUp 앱 패키지 상수화

--- core/src/main/java/net/m3mobile/core/inspection/InspectStartUpVersionSatisfied.kt
+++ core/src/main/java/net/m3mobile/core/inspection/InspectStartUpVersionSatisfied.kt
@@ -4,9 +4,12 @@ import net.m3mobile.core.InternalM3Api
 import net.m3mobile.core.source.StartUpVersionMapSource
 import java.util.ServiceLoader
 
+private const val STARTUP_APP_NAME = "StartUp"
+private const val STARTUP_APP_PACKAGE = "com.m3.startup"
+
 @InternalM3Api
 class InspectStartUpVersionSatisfied : InspectAppVersionSatisfied<StartUpVersionMapSource>() {
     override val serviceLoader = ServiceLoader.load(StartUpVersionMapSource::class.java)
-    override val appName = "StartUp"
-    override val appPackage = "com.m3.startup"
+    override val appName = STARTUP_APP_NAME
+    override val appPackage = STARTUP_APP_PACKAGE
 }
