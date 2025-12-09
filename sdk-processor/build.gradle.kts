d55b591197ce2512e83c5d3f99d8428229ee5446
Thirfir <thirfir@naver.com>
2025-12-09 13:23:43 +0900

    chore: KSP 모듈 toolchain 설정

--- sdk-processor/build.gradle.kts
+++ sdk-processor/build.gradle.kts
@@ -1,18 +1,9 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     kotlin("jvm")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
 kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_1_8
-    }
+    jvmToolchain(8)
 }
 
 dependencies {
